052f9ddad974839a51b7ed90140b2e9d343faf4c
karimliaghabala <agabala.93@gmail.com>
2023-11-01 01:05:12 +0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import './app.css'
+import Home from './Components/home/Home'
+import Navbar from './Components/navbar/Navbar'
+import Main from './Components/main/Main'
+import Footer from './Components/footer/Footer'
 
 const App = () => {
   return (
-    <h2>App</h2>
+    <>
+    <Navbar/>
+    <Home/>
+    <Main/>
+    <Footer/>
+    
+    </>
   )
 }
 
