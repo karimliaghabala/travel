f5846076e9c1135d75ee300744fe9d49c2ee05c5
karimliaghabala <agabala.93@gmail.com>
2023-11-01 14:51:43 +0400

    add navbar

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { PiDotsNineBold } from "react-icons/pi";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <section className="navbarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <h1>
+              {" "}
+              <MdOutlineTravelExplore className="icon" />
+              Travel
+            </h1>
+          </a>
+        </div>
+        <section className="navbar">
+          <ul className="navList flex ">
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Home
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Packages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Shop
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                About
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Pages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                News
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Contact
+              </a>
+            </li>
+            <button className="btn">
+              <a href="#"> BOOK NOW</a>
+            </button>
+          </ul>
+          <div className="closeNavbar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </section>
+        <div className="toggleNavbar">
+          <PiDotsNineBold className="icon" />
+        </div>
+      </header>
+    </section>
+  );
+};
 
-export default Navbar
+export default Navbar;
